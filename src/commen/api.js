8c3653501b1c15f8acb8344451192656adc1486c
jerinjerinjerin <133647407+jerinjerinjerin@users.noreply.github.com>
2024-08-10 14:22:21 +0530

    Update api.js

--- src/commen/api.js
+++ src/commen/api.js
@@ -1,4 +1,4 @@
-const backendDomin = 'https://herbal-ecommerce-backend-j4cv.vercel.app'
+const backendDomin = 'https://herbal-ecommerce-backend-7q1z.vercel.app'
 // const backendDomin = 'http://localhost:8080'
 
 
